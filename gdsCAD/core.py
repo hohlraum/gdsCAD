d65c5b609025f7334c8726080c129a07b4101bdf
Andrew Mark <mark@is.mpg.de>
2014-10-06 11:12:34 +0200

    Closed boundary check failed for point lists specified as lists or tuples. Added a cast to array first.

--- gdsCAD/core.py
+++ gdsCAD/core.py
@@ -255,6 +255,7 @@ class Boundary(ElementBase):
     show=_show
     
     def __init__(self, points, layer=None, datatype=None, verbose=False) :
+        points = np.asarray(points)
         if (points[0] != points[-1]).any():
             points = np.concatenate((points, [points[0]]))
 
