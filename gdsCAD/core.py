hohlraum/gdsCAD

commit bf6c05df2838774d1667bf48bfd0f20e220fe1e5

Close boundaries by duplicating first point to last point if necessary

gdsCAD/core.py
@@ -255,6 +255,9 @@ class Boundary(ElementBase):
     show=_show
     
     def __init__(self, points, layer=None, datatype=None, verbose=False) :
+        if (points[0] != points[-1]).any():
+            points = np.concatenate((points, [points[0]]))
+
         ElementBase.__init__(self, points)
 
         if verbose and 8191 >= self.points.shape[0] > 199:

commit e1477513aa3c99d15149e0322cee7582f480750b

Fix Boundary creation for non-numpy coordinate lists
Commit bf6c05df283 introduced a regression if the point list
is not a numpy array.

gdsCAD/core.py
@@ -102,7 +102,7 @@ class ElementBase(object):
         return {'color': color}
 
     def __init__(self, points):
-        self._points = np.array(points)
+        self._points = np.asarray(points)
         self._bbox = None
 
     @property
@@ -255,6 +255,7 @@ class Boundary(ElementBase):
     show=_show
     
     def __init__(self, points, layer=None, datatype=None, verbose=False) :
+        points = np.asarray(points)
         if (points[0] != points[-1]).any():
             points = np.concatenate((points, [points[0]]))
 