712dc4db31a64cf1db9789244ea48f9c49bb6173
Tom Varga <tomvarga@gmail.com>
2015-03-04 13:44:26 -0500

    Replaced missing lines in Path.artist done right

--- gdsCAD/core.py
+++ gdsCAD/core.py
@@ -574,7 +574,7 @@ class Path(ElementBase):
         cap_style = {0:2, 1:1, 2:3}
         points=[tuple(p) for p in self.points]
         lines = shapely.geometry.LineString(points)
-        poly = lines.buffer(self.width/2., cap_style=cap_style[self.pathtype], join_style=3)
+        poly = lines.buffer(self.width/2., cap_style=cap_style[self.pathtype], join_style=2, mitre_limit=np.sqrt(2))
 
         return [descartes.PolygonPatch(poly, lw=0, **self._layer_properties(self.layer))]
 
